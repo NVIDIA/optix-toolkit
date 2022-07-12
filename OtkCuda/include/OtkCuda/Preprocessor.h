758dfb88423557d01f77e5c4f4b232f3c49e02bf
Mark Leone <mleone@nvidia.com>
2022-07-12 13:28:20 -0600

    Include BuildConfig.cmake after project(CXX) Replace SUTIL_ with OTK_ in preprocessor definitions.

--- OtkCuda/include/OtkCuda/Preprocessor.h
+++ OtkCuda/include/OtkCuda/Preprocessor.h
@@ -30,12 +30,12 @@
 #pragma once
 
 #if defined(__CUDACC__) || defined(__CUDABE__)
-#    define SUTIL_HOSTDEVICE __host__ __device__
-#    define SUTIL_INLINE __forceinline__
+#    define OTK_HOSTDEVICE __host__ __device__
+#    define OTK_INLINE __forceinline__
 #    define CONST_STATIC_INIT( ... )
 #else
-#    define SUTIL_HOSTDEVICE
-#    define SUTIL_INLINE inline
+#    define OTK_HOSTDEVICE
+#    define OTK_INLINE inline
 #    define CONST_STATIC_INIT( ... ) = __VA_ARGS__
 #endif
 
