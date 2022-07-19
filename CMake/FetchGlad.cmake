e538248e874c2377655d61dec87d01259bfcd5d5
Mark Leone <mleone@nvidia.com>
2022-07-19 15:45:25 -0600

    Explicitly configure GLAD.

--- CMake/FetchGlad.cmake
+++ CMake/FetchGlad.cmake
@@ -35,6 +35,13 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/Dav1dde/glad
   GIT_TAG v0.1.36
 )
-FetchContent_MakeAvailable(glad)
+FetchContent_GetProperties(glad)
+if(NOT glad_POPULATED)
+    FetchContent_Populate(glad)
+    set(GLAD_PROFILE "core" CACHE STRING "OpenGL profile")
+    set(GLAD_API "gl=" CACHE STRING "API type/version pairs, like \"gl=3.2,gles=\", no version means latest")
+    set(GLAD_GENERATOR "c" CACHE STRING "Language to generate the binding for")
+    add_subdirectory(${glad_SOURCE_DIR} ${glad_BINARY_DIR})
+endif()
 
 set_target_properties(glad PROPERTIES POSITION_INDEPENDENT_CODE ON)
