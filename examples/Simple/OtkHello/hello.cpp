94628f28f05e6b19b4c956b53d06bf6d37db1ace
Mark Leone <mleone@nvidia.com>
2022-12-05 12:03:06 -0700

    Added a simple OptiX example (OtkHello).
    Fix copy/paste errors in OtkHello CMake file.
    
    Fix malformed target properties for otkHello.

---
+++ examples/Simple/OtkHello/hello.cpp
@@ -0,0 +1,294 @@
+//
+// Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include "hello.h"
+
+#include <OptiXToolkit/Gui/CUDAOutputBuffer.h>
+#include <OptiXToolkit/Gui/Window.h>
+#include <OptiXToolkit/Util/Exception.h>
+
+#include <optix.h>
+#include <optix_function_table_definition.h>
+#include <optix_stack_size.h>
+#include <optix_stubs.h>
+
+#include <cuda_runtime.h>
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+extern "C" char draw_solid_color_ptx[];  // generated via CMake by embed_ptx.
+
+template <typename T>
+struct SbtRecord
+{
+    __align__( OPTIX_SBT_RECORD_ALIGNMENT ) char header[OPTIX_SBT_RECORD_HEADER_SIZE];
+    T data;
+};
+
+typedef SbtRecord<RayGenData> RayGenSbtRecord;
+typedef SbtRecord<int>        MissSbtRecord;
+
+
+void printUsageAndExit( const char* argv0 )
+{
+    std::cerr << "Usage  : " << argv0 << " [options]\n";
+    std::cerr << "Options: --file | -f <filename>      Specify file for image output\n";
+    std::cerr << "         --help | -h                 Print this usage message\n";
+    std::cerr << "         --dim=<width>x<height>      Set image dimensions; defaults to 512x384\n";
+    exit( 1 );
+}
+
+
+static void context_log_cb( unsigned int level, const char* tag, const char* message, void* /*cbdata */ )
+{
+    std::cerr << "[" << std::setw( 2 ) << level << "][" << std::setw( 12 ) << tag << "]: " << message << "\n";
+}
+
+int main( int argc, char* argv[] )
+{
+    std::string outfile;
+    int         width  = 512;
+    int         height = 384;
+
+    for( int i = 1; i < argc; ++i )
+    {
+        const std::string arg( argv[i] );
+        if( arg == "--help" || arg == "-h" )
+        {
+            printUsageAndExit( argv[0] );
+        }
+        else if( arg == "--file" || arg == "-f" )
+        {
+            if( i < argc - 1 )
+            {
+                outfile = argv[++i];
+            }
+            else
+            {
+                printUsageAndExit( argv[0] );
+            }
+        }
+        else if( arg.substr( 0, 6 ) == "--dim=" )
+        {
+            const std::string dims_arg = arg.substr( 6 );
+            otk::parseDimensions( dims_arg.c_str(), width, height );
+        }
+        else
+        {
+            std::cerr << "Unknown option '" << arg << "'\n";
+            printUsageAndExit( argv[0] );
+        }
+    }
+
+    try
+    {
+        //
+        // Initialize CUDA and create OptiX context
+        //
+        OptixDeviceContext context = nullptr;
+        {
+            // Initialize CUDA
+            CUDA_CHECK( cudaFree( 0 ) );
+
+            CUcontext cuCtx = 0;  // zero means take the current context
+            OPTIX_CHECK( optixInit() );
+            OptixDeviceContextOptions options = {};
+            options.logCallbackFunction       = &context_log_cb;
+            options.logCallbackLevel          = 4;
+            OPTIX_CHECK( optixDeviceContextCreate( cuCtx, &options, &context ) );
+        }
+
+        //
+        // Create module
+        //
+        OptixModule                 module                   = nullptr;
+        OptixPipelineCompileOptions pipeline_compile_options = {};
+        {
+            OptixModuleCompileOptions module_compile_options = {};
+#if !defined( NDEBUG )
+            module_compile_options.optLevel   = OPTIX_COMPILE_OPTIMIZATION_LEVEL_0;
+            module_compile_options.debugLevel = OPTIX_COMPILE_DEBUG_LEVEL_FULL;
+#endif
+            pipeline_compile_options.usesMotionBlur        = false;
+            pipeline_compile_options.traversableGraphFlags = OPTIX_TRAVERSABLE_GRAPH_FLAG_ALLOW_SINGLE_LEVEL_INSTANCING;
+            pipeline_compile_options.numPayloadValues      = 2;
+            pipeline_compile_options.numAttributeValues    = 2;
+            pipeline_compile_options.exceptionFlags = OPTIX_EXCEPTION_FLAG_NONE;  // TODO: should be OPTIX_EXCEPTION_FLAG_STACK_OVERFLOW;
+            pipeline_compile_options.pipelineLaunchParamsVariableName = "params";
+
+            OPTIX_CHECK_LOG2( optixModuleCreateFromPTX( context, &module_compile_options, &pipeline_compile_options, draw_solid_color_ptx,
+                                                        ::strlen( draw_solid_color_ptx ), LOG, &LOG_SIZE, &module ) );
+        }
+
+        //
+        // Create program groups, including NULL miss and hitgroups
+        //
+        OptixProgramGroup raygen_prog_group = nullptr;
+        OptixProgramGroup miss_prog_group   = nullptr;
+        {
+            OptixProgramGroupOptions program_group_options = {};  // Initialize to zeros
+
+            OptixProgramGroupDesc raygen_prog_group_desc    = {};  //
+            raygen_prog_group_desc.kind                     = OPTIX_PROGRAM_GROUP_KIND_RAYGEN;
+            raygen_prog_group_desc.raygen.module            = module;
+            raygen_prog_group_desc.raygen.entryFunctionName = "__raygen__draw_solid_color";
+            OPTIX_CHECK_LOG2( optixProgramGroupCreate( context, &raygen_prog_group_desc,
+                                                       1,  // num program groups
+                                                       &program_group_options, LOG, &LOG_SIZE, &raygen_prog_group ) );
+
+            // Leave miss group's module and entryfunc name null
+            OptixProgramGroupDesc miss_prog_group_desc = {};
+            miss_prog_group_desc.kind                  = OPTIX_PROGRAM_GROUP_KIND_MISS;
+            OPTIX_CHECK_LOG2( optixProgramGroupCreate( context, &miss_prog_group_desc,
+                                                       1,  // num program groups
+                                                       &program_group_options, LOG, &LOG_SIZE, &miss_prog_group ) );
+        }
+
+        //
+        // Link pipeline
+        //
+        OptixPipeline pipeline = nullptr;
+        {
+            const uint32_t    max_trace_depth  = 0;
+            OptixProgramGroup program_groups[] = {raygen_prog_group};
+
+            OptixPipelineLinkOptions pipeline_link_options = {};
+            pipeline_link_options.maxTraceDepth            = max_trace_depth;
+            OPTIX_CHECK_LOG2( optixPipelineCreate( context, &pipeline_compile_options, &pipeline_link_options,
+                                                   program_groups, sizeof( program_groups ) / sizeof( program_groups[0] ),
+                                                   LOG, &LOG_SIZE, &pipeline ) );
+
+            OptixStackSizes stack_sizes = {};
+            for( auto& prog_group : program_groups )
+            {
+                OPTIX_CHECK( optixUtilAccumulateStackSizes( prog_group, &stack_sizes ) );
+            }
+
+            uint32_t direct_callable_stack_size_from_traversal;
+            uint32_t direct_callable_stack_size_from_state;
+            uint32_t continuation_stack_size;
+            OPTIX_CHECK( optixUtilComputeStackSizes( &stack_sizes, max_trace_depth,
+                                                     0,  // maxCCDepth
+                                                     0,  // maxDCDEpth
+                                                     &direct_callable_stack_size_from_traversal,
+                                                     &direct_callable_stack_size_from_state, &continuation_stack_size ) );
+            OPTIX_CHECK( optixPipelineSetStackSize( pipeline, direct_callable_stack_size_from_traversal,
+                                                    direct_callable_stack_size_from_state, continuation_stack_size,
+                                                    2  // maxTraversableDepth
+                                                    ) );
+        }
+
+        //
+        // Set up shader binding table
+        //
+        OptixShaderBindingTable sbt = {};
+        {
+            CUdeviceptr  raygen_record;
+            const size_t raygen_record_size = sizeof( RayGenSbtRecord );
+            CUDA_CHECK( cudaMalloc( reinterpret_cast<void**>( &raygen_record ), raygen_record_size ) );
+            RayGenSbtRecord rg_sbt;
+            OPTIX_CHECK( optixSbtRecordPackHeader( raygen_prog_group, &rg_sbt ) );
+            rg_sbt.data = {0.462f, 0.725f, 0.f};
+            CUDA_CHECK( cudaMemcpy( reinterpret_cast<void*>( raygen_record ), &rg_sbt, raygen_record_size, cudaMemcpyHostToDevice ) );
+
+            CUdeviceptr miss_record;
+            size_t      miss_record_size = sizeof( MissSbtRecord );
+            CUDA_CHECK( cudaMalloc( reinterpret_cast<void**>( &miss_record ), miss_record_size ) );
+            RayGenSbtRecord ms_sbt;
+            OPTIX_CHECK( optixSbtRecordPackHeader( miss_prog_group, &ms_sbt ) );
+            CUDA_CHECK( cudaMemcpy( reinterpret_cast<void*>( miss_record ), &ms_sbt, miss_record_size, cudaMemcpyHostToDevice ) );
+
+            sbt.raygenRecord            = raygen_record;
+            sbt.missRecordBase          = miss_record;
+            sbt.missRecordStrideInBytes = sizeof( MissSbtRecord );
+            sbt.missRecordCount         = 1;
+        }
+
+        otk::CUDAOutputBuffer<uchar4> output_buffer( otk::CUDAOutputBufferType::CUDA_DEVICE, width, height );
+
+        //
+        // launch
+        //
+        {
+            CUstream stream;
+            CUDA_CHECK( cudaStreamCreate( &stream ) );
+
+            Params params;
+            params.image       = output_buffer.map();
+            params.image_width = width;
+
+            CUdeviceptr d_param;
+            CUDA_CHECK( cudaMalloc( reinterpret_cast<void**>( &d_param ), sizeof( Params ) ) );
+            CUDA_CHECK( cudaMemcpy( reinterpret_cast<void*>( d_param ), &params, sizeof( params ), cudaMemcpyHostToDevice ) );
+
+            OPTIX_CHECK( optixLaunch( pipeline, stream, d_param, sizeof( Params ), &sbt, width, height, /*depth=*/1 ) );
+            CUDA_SYNC_CHECK();
+
+            output_buffer.unmap();
+            CUDA_CHECK( cudaFree( reinterpret_cast<void*>( d_param ) ) );
+        }
+
+        //
+        // Display results
+        //
+        {
+            otk::ImageBuffer buffer;
+            buffer.data         = output_buffer.getHostPointer();
+            buffer.width        = width;
+            buffer.height       = height;
+            buffer.pixel_format = otk::BufferImageFormat::UNSIGNED_BYTE4;
+            if( outfile.empty() )
+                otk::displayBufferWindow( argv[0], buffer );
+            else
+                otk::saveImage( outfile.c_str(), buffer, false );
+        }
+
+        //
+        // Cleanup
+        //
+        {
+            CUDA_CHECK( cudaFree( reinterpret_cast<void*>( sbt.raygenRecord ) ) );
+            CUDA_CHECK( cudaFree( reinterpret_cast<void*>( sbt.missRecordBase ) ) );
+
+            OPTIX_CHECK( optixPipelineDestroy( pipeline ) );
+            OPTIX_CHECK( optixProgramGroupDestroy( miss_prog_group ) );
+            OPTIX_CHECK( optixProgramGroupDestroy( raygen_prog_group ) );
+            OPTIX_CHECK( optixModuleDestroy( module ) );
+
+            OPTIX_CHECK( optixDeviceContextDestroy( context ) );
+        }
+    }
+    catch( std::exception& e )
+    {
+        std::cerr << "Caught exception: " << e.what() << "\n";
+        return 1;
+    }
+    return 0;
+}
