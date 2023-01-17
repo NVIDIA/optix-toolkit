0ab0e8e1bd3aac53697c07b9885715089b5cff16
Mark Leone <mleone@nvidia.com>
2023-01-17 16:22:28 -0700

    Move PyOptix and and ShaderUtil to submodules.

--- PyOptiX/test/conftest.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022 NVIDIA CORPORATION All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
-
-
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'util'))
