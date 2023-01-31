291daf60418b73c4d38151904d47ba1ce06a15bd
Richard Thomson <rthomson@nvidia.com>
2023-01-31 10:00:13 -0700

    Avoid default arguments with virtual functions

--- tests/TestCheckerBoardImage.cpp
+++ tests/TestCheckerBoardImage.cpp
@@ -50,7 +50,7 @@ TEST_F( TestCheckerBoardImage, ReadTile )
     std::vector<float4> buffer( tileWidth * tileHeight );
 
     float4* texels = buffer.data();
-    ASSERT_NO_THROW( image.readTile( reinterpret_cast<char*>( texels ), mipLevel, 1, 1, tileWidth, tileHeight ) );
+    ASSERT_NO_THROW( image.readTile( reinterpret_cast<char*>( texels ), mipLevel, 1, 1, tileWidth, tileHeight, nullptr ) );
 
     // For now, we print the pixels for visual inspection.
     for( unsigned int y = 0; y < tileHeight; ++y )
