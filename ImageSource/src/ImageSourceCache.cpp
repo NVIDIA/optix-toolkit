452a8bd4edbfaf3c5e5941c2f85a7431364c5afa
Mark Leone <mleone@nvidia.com>
2024-03-13 15:04:04 -0600

    Added ImageSourceCache.

---
+++ ImageSource/src/ImageSourceCache.cpp
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include <OptiXToolkit/ImageSource/ImageSourceCache.h>
+
+namespace imageSource {
+
+std::shared_ptr<ImageSource> ImageSourceCache::get( const std::string& filename, const std::string& directory )
+{
+    // Use a cached ImageSource if possible.
+    auto it = m_map.find( Key( filename, directory ) );
+    if( it != m_map.end() )
+        return it->second;
+
+    // Create a new ImageSource and cache it.
+    std::shared_ptr<ImageSource> imageSource = createImageSource( filename, directory );
+
+    m_map[Key( filename, directory )] = imageSource;
+    return imageSource;
+}
+
+}  // namespace imageSource
