9092f2ada94e6cfaf704f1ca257ee43ba46db838
Richard Thomson <rthomson@nvidia.com>
2024-03-19 15:23:47 -0600

    Add predicate to determine if debugging at launch index

--- include/OptiXToolkit/ShaderUtil/DebugLocation.h
+++ include/OptiXToolkit/ShaderUtil/DebugLocation.h
@@ -67,6 +67,16 @@ static __forceinline__ __device__ bool inDebugWindow( const uint3& launchIndex, 
 
 }  // namespace debugDetail
 
+/// atDebugIndex
+///
+/// Returns true if debugging enabled, debug pixel set and it matches the given launchIndex.
+/// This is useful when dumping debug information from raygen programs.
+///
+inline bool atDebugIndex( const DebugLocation& debug, const uint3 launchIndex = optixGetLaunchIndex() )
+{
+    return debug.enabled && debug.debugIndexSet && debug.debugIndex == launchIndex;
+}
+
 /// debugInfoDump
 ///
 /// Invoke a functor at a specific launch index for debugging purposes.  THe functor takes the OptiX launch index
