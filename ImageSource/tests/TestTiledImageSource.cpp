6016ac68f959269aaf56f14fd8463a3fdc10f86d
Richard Thomson <rthomson@nvidia.com>
2024-03-27 14:49:54 -0600

    Cover TiledImageSource with more tests for partial tile cases

--- ImageSource/tests/TestTiledImageSource.cpp
+++ ImageSource/tests/TestTiledImageSource.cpp
@@ -55,6 +55,11 @@ class TestTiledImageSource : public Test
     ExpectationSet expectCreate();
     void           create();
     ExpectationSet expectOpen();
+    unsigned int   getPixelSizeInBytes() const
+    {
+        return imageSource::getBytesPerChannel( m_baseInfo.format ) * m_baseInfo.numChannels;
+    }
+    void expectLevelZeroFilledAfter( const ExpectationSet& before );
 
     otk::testing::MockImageSourcePtr m_baseImage{ std::make_shared<otk::testing::MockImageSource>() };
     imageSource::TextureInfo         m_baseInfo{};
@@ -93,6 +98,30 @@ ExpectationSet TestTiledImageSource::expectOpen()
     open += EXPECT_CALL( *m_baseImage, open( IsNull() ) ).After( create );
     open += EXPECT_CALL( *m_baseImage, getInfo() ).After( create ).WillOnce( ReturnRef( m_baseInfo ) );
     return open;
+}
+
+void TestTiledImageSource::expectLevelZeroFilledAfter( const ExpectationSet& before )
+{
+    const unsigned int mipLevel{ 0 };
+    const unsigned int mipLevelWidth{ m_baseInfo.width };
+    const unsigned int mipLevelHeight{ m_baseInfo.height };
+    const unsigned int pixelSizeInBytes = getPixelSizeInBytes();
+    const auto         fillMipLevel     = [=]( char* dest, unsigned int /*mipLevel*/, unsigned int expectedWidth,
+                                   unsigned int expectedHeight, CUstream /*stream*/ ) {
+        int val{};
+        for( unsigned int y = 0; y < expectedHeight; ++y )
+        {
+            for( unsigned int x = 0; x < expectedWidth; ++x )
+            {
+                std::fill( &dest[0], &dest[pixelSizeInBytes], static_cast<char>( val & 0xFF ) );
+                ++val;
+                dest += pixelSizeInBytes;
+            }
+        }
+    };
+    EXPECT_CALL( *m_baseImage, readMipLevel( NotNull(), mipLevel, mipLevelWidth, mipLevelHeight, m_stream ) )
+        .After( before )
+        .WillOnce( DoAll( fillMipLevel, Return( true ) ) );
 }
 
 }  // namespace
@@ -161,48 +190,163 @@ TEST_F( TestTiledImageSource, getInfoWithoutOpenIsInvalid )
     ASSERT_FALSE( result.isValid );
 }
 
-TEST_F( TestTiledImageSource, readTileSourcesDataFromReadMipLevel )
+inline int charAsInt( char c )
 {
-    expectOpen();
-    create();
-    const unsigned int mipLevel{ 0 };
-    const unsigned int mipLevelWidth{ m_baseInfo.width };
-    const unsigned int mipLevelHeight{ m_baseInfo.height };
-    const unsigned int pixelSizeInBytes{ imageSource::getBytesPerChannel( m_baseInfo.format ) * m_baseInfo.numChannels };
-    const auto fillMipLevel = [=]( char* dest, unsigned int /*mipLevel*/, unsigned int expectedWidth,
-                                   unsigned int expectedHeight, CUstream /*stream*/ ) {
-        int val{};
-        for( unsigned int y = 0; y < expectedHeight; ++y )
+    return static_cast<unsigned char>( c );
+};
+
+MATCHER_P4( hasPixelRowValueSequence, y, rowWidth, pixelSizeInBytes, start, "" )
+{
+    const std::vector<char> &dest = arg;
+    unsigned int val = start;
+    bool result{true};
+    for( unsigned int x = 0; x < rowWidth; ++x )
+    {
+        for( unsigned int c = 0; c < pixelSizeInBytes; ++c )
         {
-            for( unsigned int x = 0; x < expectedWidth; ++x )
+            const char expected = static_cast<char>( val & 0xFF );
+            const char actual = dest[y * rowWidth * pixelSizeInBytes + x * pixelSizeInBytes + c];
+            if ( expected != actual )
             {
-                std::fill( &dest[0], &dest[pixelSizeInBytes], static_cast<char>( val & 0xFF ) );
-                ++val;
-                dest += pixelSizeInBytes;
+                if (!result)
+                {
+                    *result_listener << "; ";
+                }
+                *result_listener << "[" << x << "," << y << "][" << c << "]: expected " << charAsInt(expected) << ", got " << charAsInt(actual);
+                result = false;
             }
         }
-    };
-    EXPECT_CALL( *m_baseImage, readMipLevel( NotNull(), mipLevel, mipLevelWidth, mipLevelHeight, m_stream ) )
-        .WillOnce( DoAll( fillMipLevel, Return( true ) ) );
+        ++val;
+    }
+    if (result)
+    {
+        *result_listener << "matches pattern";
+    }
+    return result;
+}
+
+MATCHER_P3( hasPixelRowZeroSequence, y, rowWidth, pixelSizeInBytes, "")
+{
+    const std::vector<char> &dest = arg;
+    bool result{true};
+    for( unsigned int x = 0; x < rowWidth; ++x )
+    {
+        for( unsigned int c = 0; c < pixelSizeInBytes; ++c )
+        {
+            const char actual = dest[y * rowWidth * pixelSizeInBytes + x * pixelSizeInBytes + c];
+            if ( actual != 0 )
+            {
+                if (!result)
+                {
+                    *result_listener << "; ";
+                }
+                *result_listener << "[" << x << "," << y << "][" << c << "]: expected zero, got " << charAsInt(actual);
+                result = false;
+            }
+        }
+    }
+    if (result)
+    {
+        *result_listener << "matches zeros";
+    }
+    return result;
+}
+
+TEST_F( TestTiledImageSource, readFullTileSourcesDataFromReadMipLevel )
+{
+    ExpectationSet before = expectOpen();
+    create();
+    expectLevelZeroFilledAfter( before );
     imageSource::TextureInfo info{};
     m_tiledImage->open( &info );
     ASSERT_TRUE( info.isValid );
-
-    const imageSource::Tile tile{ 0, 0, 64, 64 };
+    const unsigned int      tileSize{ 64 };
+    const imageSource::Tile tile{ 0, 0, tileSize, tileSize };
     std::vector<char>       dest;
     dest.resize( tile.width * tile.height * 4 );
-    ASSERT_TRUE( m_tiledImage->readTile( dest.data(), mipLevel, tile, m_stream ) );
 
-    for( unsigned int y = 0; y < 64; ++y )
+    ASSERT_TRUE( m_tiledImage->readTile( dest.data(), 0, tile, m_stream ) );
+
+    const unsigned int pixelSizeInBytes = getPixelSizeInBytes();
+    for( unsigned int y = 0; y < tile.height; ++y )
     {
-        int val = y * m_baseInfo.width;
-        for( unsigned int x = 0; x < 64; ++x )
+        unsigned int val = y * m_baseInfo.width;
+        EXPECT_THAT( dest, hasPixelRowValueSequence( y, tile.width, pixelSizeInBytes, val ) );
+    }
+}
+
+TEST_F( TestTiledImageSource, readPartialTileAtWidthBoundary )
+{
+    const unsigned int tileSize{ 32 };
+    m_baseInfo.width      = 2 * tileSize + tileSize / 2;  // 2.5 tiles wide
+    m_baseInfo.height     = 2 * tileSize;                 // 2 tiles high
+    ExpectationSet before = expectOpen();
+    create();
+    expectLevelZeroFilledAfter( before );
+    imageSource::TextureInfo info{};
+    m_tiledImage->open( &info );
+    ASSERT_TRUE( info.isValid );
+    const imageSource::Tile tile{ 2, 0, tileSize, tileSize };
+    std::vector<char>       dest;
+    dest.resize( tile.width * tile.height * 4 );
+
+    ASSERT_TRUE( m_tiledImage->readTile( dest.data(), 0, tile, m_stream ) );
+
+    const unsigned int pixelSizeInBytes = getPixelSizeInBytes();
+    for( unsigned int y = 0; y < tile.height; ++y )
+    {
+        unsigned int val = y * m_baseInfo.width + tile.x * tile.width;
+        for( unsigned int x = 0; x < tile.width; ++x )
         {
-            for( unsigned int c = 0; c < pixelSizeInBytes; ++c )
+            if( x < tile.width / 2 )
             {
-                EXPECT_EQ( static_cast<char>( val & 0xFF ), dest[y * tile.width * pixelSizeInBytes + x * pixelSizeInBytes + c] );
+                for( unsigned int c = 0; c < pixelSizeInBytes; ++c )
+                {
+                    EXPECT_EQ( static_cast<char>( val & 0xFF ), dest[y * tile.width * pixelSizeInBytes + x * pixelSizeInBytes + c] )
+                        << "[" << x << ", " << y << "] channel " << c;
+                }
+            }
+            else
+            {
+                for( unsigned int c = 0; c < pixelSizeInBytes; ++c )
+                {
+                    EXPECT_EQ( 0, dest[y * tile.width * pixelSizeInBytes + x * pixelSizeInBytes + c] )
+                        << "[" << x << ", " << y << "] channel " << c;
+                }
             }
             ++val;
+        }
+    }
+}
+
+TEST_F( TestTiledImageSource, readPartialTileAtHeightBoundary )
+{
+    const unsigned int tileSize{ 32 };
+    m_baseInfo.width      = 2 * tileSize;                 // 2 tiles high
+    m_baseInfo.height     = 2 * tileSize + tileSize / 2;  // 2.5 tiles wide
+    ExpectationSet before = expectOpen();
+    create();
+    expectLevelZeroFilledAfter( before );
+    imageSource::TextureInfo info{};
+    m_tiledImage->open( &info );
+    ASSERT_TRUE( info.isValid );
+    const imageSource::Tile tile{ 0, 2, tileSize, tileSize };
+    std::vector<char>       dest;
+    dest.resize( tile.width * tile.height * 4 );
+
+    ASSERT_TRUE( m_tiledImage->readTile( dest.data(), 0, tile, m_stream ) );
+
+    const unsigned int pixelSizeInBytes = getPixelSizeInBytes();
+    for( unsigned int y = 0; y < tile.height; ++y )
+    {
+        unsigned int val = ( y + tile.y * tile.height ) * m_baseInfo.width;
+        if( y < tile.height / 2 )
+        {
+            EXPECT_THAT( dest, hasPixelRowValueSequence( y, tile.width, pixelSizeInBytes, val ) );
+        }
+        else
+        {
+            EXPECT_THAT( dest, hasPixelRowZeroSequence( y, tile.width, pixelSizeInBytes ) );
         }
     }
 }
