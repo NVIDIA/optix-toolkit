814f03b9128e4360ab9b6a3b727b7565ca00ae94
Richard Thomson <rthomson@nvidia.com>
2024-05-07 15:27:35 -0600

    Revert "Use OVERRIDE_FIND_PACKAGE with FetchContent instead of FOUND variable hack"
    This reverts commit f36db470e9a21a290e4b26c6faf554dd9abcbb7c.

--- CMake/Fetchglog.cmake
+++ CMake/Fetchglog.cmake
@@ -44,8 +44,9 @@ FetchContent_Declare(
   GIT_TAG a6a166db069520dbbd653c97c2e5b12e08a8bb26 # v0.3.5
   GIT_PROGRESS TRUE
   PATCH_COMMAND ${CMAKE_COMMAND} -DGITCOMMAND:PATH=${GITCOMMAND} -DPATCHFILE:PATH=${CMAKE_CURRENT_LIST_DIR}/glog-build-parameters.patch.txt -P ${CMAKE_CURRENT_LIST_DIR}/GitPatch.cmake
-  OVERRIDE_FIND_PACKAGE
 )
 FetchContent_MakeAvailable(glog)
+# Let find_package know we have it
+set(glog_FOUND ON PARENT_SCOPE)
 
 set_target_properties(glog PROPERTIES FOLDER "ThirdParty")
