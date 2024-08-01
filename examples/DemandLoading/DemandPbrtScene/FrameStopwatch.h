c538546d738bc87914a4153ec4903987715731b9
Richard Thomson <rthomson@nvidia.com>
2024-08-01 13:57:45 -0600

    Extract Header FrameStopwatch.h from PbrtScene.{h,cpp}

---
+++ examples/DemandLoading/DemandPbrtScene/FrameStopwatch.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <chrono>
+
+namespace demandPbrtScene {
+
+class FrameStopwatch
+{
+  public:
+    FrameStopwatch( bool interactive )
+        : m_interactive( interactive )
+    {
+    }
+
+    void start() { m_frameStart = Clock::now(); }
+
+    bool expired() const
+    {
+        // infinite frame budget when rendering to a file
+        if( !m_interactive )
+        {
+            return false;
+        }
+
+        const Clock::duration duration = Clock::now() - m_frameStart;
+        return duration > m_frameTime;
+    }
+
+
+  private:
+    using Clock = std::chrono::steady_clock;
+
+    bool m_interactive;
+
+    // desire 60 fps
+    Clock::duration                m_frameTime{ std::chrono::milliseconds( 1000 / 60 ) };
+    std::chrono::time_point<Clock> m_frameStart{};
+};
+
+}  // namespace demandPbrtScene
