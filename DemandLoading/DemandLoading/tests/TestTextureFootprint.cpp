da07f88f224789305b227afcd5b5c86064d8d930
Daniel Rushton <drushton@nvidia.com>
2024-08-07 14:35:57 -0600

    Set OptixModuleCompileOptions debugLevel and optLevel according to DNDEBUG state in TestTextureFootprint.cpp and OptiXScene.cpp to be consistent with other targets in OTK.

--- DemandLoading/DemandLoading/tests/TestTextureFootprint.cpp
+++ DemandLoading/DemandLoading/tests/TestTextureFootprint.cpp
@@ -313,9 +313,21 @@ class TextureFootprintFixture
             OptixPipelineCompileOptions pipeline_compile_options = {};
             {
                 OptixModuleCompileOptions module_compile_options = {};
-                module_compile_options.maxRegisterCount          = OPTIX_COMPILE_DEFAULT_MAX_REGISTER_COUNT;
-                module_compile_options.optLevel                  = OPTIX_COMPILE_OPTIMIZATION_DEFAULT;
-                module_compile_options.debugLevel                = OPTIX_COMPILE_DEBUG_LEVEL_DEFAULT;
+#ifdef NDEBUG
+                bool debugInfo{ false };
+#else
+                bool debugInfo{ true };
+#endif
+                module_compile_options.optLevel         = debugInfo ? OPTIX_COMPILE_OPTIMIZATION_LEVEL_0 :
+                                                                      OPTIX_COMPILE_OPTIMIZATION_DEFAULT;
+#if OPTIX_VERSION >= 70400
+                module_compile_options.debugLevel       = debugInfo ? OPTIX_COMPILE_DEBUG_LEVEL_FULL :
+                                                                      OPTIX_COMPILE_DEBUG_LEVEL_MINIMAL;
+#else
+                module_compile_options.debugLevel       = debugInfo ? OPTIX_COMPILE_DEBUG_LEVEL_FULL :
+                                                                      OPTIX_COMPILE_DEBUG_LEVEL_LINEINFO;
+#endif
+                module_compile_options.maxRegisterCount = OPTIX_COMPILE_DEFAULT_MAX_REGISTER_COUNT;
 
                 pipeline_compile_options.usesMotionBlur        = false;
                 pipeline_compile_options.traversableGraphFlags = OPTIX_TRAVERSABLE_GRAPH_FLAG_ALLOW_SINGLE_LEVEL_INSTANCING;
