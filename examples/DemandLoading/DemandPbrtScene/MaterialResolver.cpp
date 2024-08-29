a04c13d7bbc54ecf0d7255c9e75816dcd80e70d7
Richard Thomson <rthomson@nvidia.com>
2024-08-29 16:33:54 -0600

    GeometryInstance returns a vector of MaterialGroups
    Currently only the first MaterialGroup is examined; moving towards
    supporting multiple MaterialGroups per GAS.

--- examples/DemandLoading/DemandPbrtScene/MaterialResolver.cpp
+++ examples/DemandLoading/DemandPbrtScene/MaterialResolver.cpp
@@ -94,30 +94,30 @@ MaterialResolution PbrtMaterialResolver::resolveMaterial( uint_t proxyMaterialId
     if( geom.instance.primitive == GeometryPrimitive::TRIANGLE )
     {
         // phase 1 alpha map resolution
-        if( flagSet( geom.instance.groups.material.flags, MaterialFlags::ALPHA_MAP )
-            && !flagSet( geom.instance.groups.material.flags, MaterialFlags::ALPHA_MAP_ALLOCATED ) )
+        if( flagSet( geom.instance.groups[0].material.flags, MaterialFlags::ALPHA_MAP )
+            && !flagSet( geom.instance.groups[0].material.flags, MaterialFlags::ALPHA_MAP_ALLOCATED ) )
         {
-            const uint_t alphaTextureId{ m_demandTextureCache->createAlphaTextureFromFile( geom.instance.groups.alphaMapFileName ) };
-            m_sync.minAlphaTextureId                     = std::min( alphaTextureId, m_sync.minAlphaTextureId );
-            m_sync.maxAlphaTextureId                     = std::max( alphaTextureId, m_sync.maxAlphaTextureId );
-            geom.instance.groups.material.alphaTextureId = alphaTextureId;
-            geom.instance.groups.material.flags |= MaterialFlags::ALPHA_MAP_ALLOCATED;
+            const uint_t alphaTextureId{ m_demandTextureCache->createAlphaTextureFromFile( geom.instance.groups[0].alphaMapFileName ) };
+            m_sync.minAlphaTextureId                        = std::min( alphaTextureId, m_sync.minAlphaTextureId );
+            m_sync.maxAlphaTextureId                        = std::max( alphaTextureId, m_sync.maxAlphaTextureId );
+            geom.instance.groups[0].material.alphaTextureId = alphaTextureId;
+            geom.instance.groups[0].material.flags |= MaterialFlags::ALPHA_MAP_ALLOCATED;
             const size_t numProxyMaterials{ proxyMaterialId + 1 };  // ids are zero based
             grow( m_sync.partialMaterials, numProxyMaterials );
             grow( m_sync.partialUVs, numProxyMaterials );
-            m_sync.partialMaterials[proxyMaterialId].alphaTextureId = geom.instance.groups.material.alphaTextureId;
+            m_sync.partialMaterials[proxyMaterialId].alphaTextureId = geom.instance.groups[0].material.alphaTextureId;
             m_sync.partialUVs[proxyMaterialId]                      = geom.instance.devUVs;
             m_sync.topLevelInstances[geom.instanceIndex].sbtOffset  = +HitGroupIndex::PROXY_MATERIAL_TRIANGLE_ALPHA;
             if( m_options.verboseProxyMaterialResolution )
             {
                 std::cout << "Resolved proxy material id " << proxyMaterialId << " to partial alpha texture id "
-                          << geom.instance.groups.material.alphaTextureId << '\n';
+                          << geom.instance.groups[0].material.alphaTextureId << '\n';
             }
             return MaterialResolution::PARTIAL;
         }
 
         // phase 2 alpha map resolution
-        if( flagSet( geom.instance.groups.material.flags, MaterialFlags::ALPHA_MAP_ALLOCATED ) )
+        if( flagSet( geom.instance.groups[0].material.flags, MaterialFlags::ALPHA_MAP_ALLOCATED ) )
         {
             // not strictly necessary, but indicates this partial material has been resolved completely
             m_sync.partialMaterials[proxyMaterialId].alphaTextureId = 0;
@@ -125,15 +125,15 @@ MaterialResolution PbrtMaterialResolver::resolveMaterial( uint_t proxyMaterialId
         }
 
         // diffuse map resolution
-        if( flagSet( geom.instance.groups.material.flags, MaterialFlags::DIFFUSE_MAP )
-            && !flagSet( geom.instance.groups.material.flags, MaterialFlags::DIFFUSE_MAP_ALLOCATED ) )
+        if( flagSet( geom.instance.groups[0].material.flags, MaterialFlags::DIFFUSE_MAP )
+            && !flagSet( geom.instance.groups[0].material.flags, MaterialFlags::DIFFUSE_MAP_ALLOCATED ) )
         {
             const uint_t diffuseTextureId =
-                m_demandTextureCache->createDiffuseTextureFromFile( geom.instance.groups.diffuseMapFileName );
-            m_sync.minDiffuseTextureId                     = std::min( diffuseTextureId, m_sync.minDiffuseTextureId );
-            m_sync.maxDiffuseTextureId                     = std::max( diffuseTextureId, m_sync.maxDiffuseTextureId );
-            geom.instance.groups.material.diffuseTextureId = diffuseTextureId;
-            geom.instance.groups.material.flags |= MaterialFlags::DIFFUSE_MAP_ALLOCATED;
+                m_demandTextureCache->createDiffuseTextureFromFile( geom.instance.groups[0].diffuseMapFileName );
+            m_sync.minDiffuseTextureId = std::min( diffuseTextureId, m_sync.minDiffuseTextureId );
+            m_sync.maxDiffuseTextureId = std::max( diffuseTextureId, m_sync.maxDiffuseTextureId );
+            geom.instance.groups[0].material.diffuseTextureId = diffuseTextureId;
+            geom.instance.groups[0].material.flags |= MaterialFlags::DIFFUSE_MAP_ALLOCATED;
         }
     }
 
@@ -141,14 +141,14 @@ MaterialResolution PbrtMaterialResolver::resolveMaterial( uint_t proxyMaterialId
     geom.instance.instance.instanceId = m_sync.instanceMaterialIds.size();
     const uint_t materialId           = m_sync.realizedMaterials.size();
     m_sync.instanceMaterialIds.push_back( materialId );
-    m_sync.realizedMaterials.push_back( geom.instance.groups.material );
+    m_sync.realizedMaterials.push_back( geom.instance.groups[0].material );
     m_sync.realizedNormals.push_back( geom.instance.devNormals );
     m_sync.realizedUVs.push_back( geom.instance.devUVs );
     m_sync.topLevelInstances[geom.instanceIndex] = geom.instance.instance;
     if( m_options.verboseProxyMaterialResolution )
     {
         std::cout << "Resolved proxy material id " << proxyMaterialId << " for instance id " << geom.instance.instance.instanceId
-                  << ( flagSet( geom.instance.groups.material.flags, MaterialFlags::DIFFUSE_MAP_ALLOCATED ) ?
+                  << ( flagSet( geom.instance.groups[0].material.flags, MaterialFlags::DIFFUSE_MAP_ALLOCATED ) ?
                            " with diffuse map" :
                            "" )
                   << '\n';
@@ -253,7 +253,7 @@ bool PbrtMaterialResolver::resolveGeometryToExistingMaterial( uint_t            
     // just for completeness's sake, mark the duplicate material's textures as having
     // been loaded, although we won't use the duplicate material after this.
     const auto markAllocated = [&]( MaterialFlags requested, MaterialFlags allocated ) {
-        MaterialFlags& flags{ geom.instance.groups.material.flags };
+        MaterialFlags& flags{ geom.instance.groups[0].material.flags };
         if( flagSet( flags, requested ) )
         {
             flags |= allocated;
@@ -307,7 +307,7 @@ bool PbrtMaterialResolver::resolveGeometryToProxyMaterial( uint_t proxyGeomId, S
 bool PbrtMaterialResolver::resolveMaterialForGeometry( uint_t proxyGeomId, const GeometryInstance& geomInstance, SceneSyncState& syncState )
 {
     // check for shared materials
-    if( const std::optional<uint_t> id = findResolvedMaterial( geomInstance.groups, syncState ); id.has_value() )
+    if( const std::optional<uint_t> id = findResolvedMaterial( geomInstance.groups[0], syncState ); id.has_value() )
     {
         return resolveGeometryToExistingMaterial( proxyGeomId, syncState, id.value(), geomInstance );
     }
--- examples/DemandLoading/DemandPbrtScene/ProgramGroups.cpp
+++ examples/DemandLoading/DemandPbrtScene/ProgramGroups.cpp
@@ -268,7 +268,7 @@ uint_t PbrtProgramGroups::getRealizedMaterialSbtOffset( const GeometryInstance& 
 
     if( instance.primitive == GeometryPrimitive::TRIANGLE )
     {
-        return getTriangleRealizedMaterialSbtOffset( instance.groups.material.flags );
+        return getTriangleRealizedMaterialSbtOffset( instance.groups[0].material.flags );
     }
     if( instance.primitive == GeometryPrimitive::SPHERE )
     {
