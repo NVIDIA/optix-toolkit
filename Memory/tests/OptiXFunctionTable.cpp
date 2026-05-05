06556da7b0fcadffecb57075331b638a0b460078
Richard Thomson <rthomson@nvidia.com>
2026-05-05 14:35:36 -0600

    Define the optix function table in testOptiXMemory
    The source file defining the function table was omitted by mistake.

---
+++ Memory/tests/OptiXFunctionTable.cpp
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+#include <optix_function_table_definition.h>
